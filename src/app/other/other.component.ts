b855857ec8ddcc192f8109af3b47e87169e99bb6
pushpe <pushpakumara.14@cse.mrt.ac.lk>
2017-04-01 17:53:40 +0530

    custom property binding work fine

--- src/app/other/other.component.ts
+++ src/app/other/other.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {PropertyBindingComponent} from "./property-binding.component";
 
 @Component({
   selector: 'app-other',
   templateUrl: './other.component.html',
-  styleUrls: ['./other.component.css']
+  styleUrls: ['./other.component.css'],
+  entryComponents: [PropertyBindingComponent]
 })
 export class OtherComponent {
   stringInterpolation = 'This is string interpolation';
   numberInterpolation = 5;
+
 }
